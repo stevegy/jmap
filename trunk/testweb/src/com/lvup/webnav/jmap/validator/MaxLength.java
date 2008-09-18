18895c818dfd4b8b2103d700ec1459cd90529ade
stevegy <stevegy@9c5c9c86-857f-11dd-9ffd-cdf95c2cae98>
2008-09-18 13:16:24 +0000

    Copied remotely

---
+++ trunk/testweb/src/com/lvup/webnav/jmap/validator/MaxLength.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright all rights reserved by LVUP Shanghai.
+ * 
+ */
+
+package com.lvup.webnav.jmap.validator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Steve Yao <steve.yao@lvup.com>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MaxLength {
+    int value() default 0;
+    
+    String formatmsg() default "formatMsg";
+
+    Message fieldName() default @Message("std.maxlength");
+
+    Message message() default @Message("std.maxlength");
+}
