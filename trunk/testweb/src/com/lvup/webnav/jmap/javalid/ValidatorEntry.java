682778750eec74bc7dc1fe9e1a03f67e01c104b8
stevegy <stevegy@9c5c9c86-857f-11dd-9ffd-cdf95c2cae98>
2008-09-29 11:38:31 +0000

    JaValid testing, but the MaxLength and MinLength failed on byte length checking.

--- trunk/testweb/src/com/lvup/webnav/jmap/javalid/ValidatorEntry.java
+++ trunk/testweb/src/com/lvup/webnav/jmap/javalid/ValidatorEntry.java
@@ -2,13 +2,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.lvup.webnav.jmap.javalid;
 
-/**
- *
- * @author steve
- */
+import org.javalid.core.AnnotationValidator;
+import org.javalid.core.AnnotationValidatorImpl;
+
 public class ValidatorEntry {
 
+    private static AnnotationValidator validator = null;
+
+    private ValidatorEntry() {
+    }
+
+    public static final AnnotationValidator getValidator() {
+        if (validator == null) {
+            validator = new AnnotationValidatorImpl();
+        }
+        return validator;
+    }
 }
+
