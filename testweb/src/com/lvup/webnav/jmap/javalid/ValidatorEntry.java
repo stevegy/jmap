d0a0a228af0cd99e04f304a7b4ecc70cca129135
stevegy <stevegy@9c5c9c86-857f-11dd-9ffd-cdf95c2cae98>
2008-09-29 09:12:50 +0000

    add the JaValid support

---
+++ testweb/src/com/lvup/webnav/jmap/javalid/ValidatorEntry.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.lvup.webnav.jmap.javalid;
+
+/**
+ *
+ * @author steve
+ */
+public class ValidatorEntry {
+
+}
