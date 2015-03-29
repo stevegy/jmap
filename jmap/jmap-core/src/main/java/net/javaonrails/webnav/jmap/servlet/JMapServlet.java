1c7ab8e8995aa4ca709ed539da964a29ccd6d65f
steve.yao@live.com <>
2015-03-29 15:00:04 +0800

    add doPut and doDelete method to Servlet handler

--- jmap/jmap-core/src/main/java/net/javaonrails/webnav/jmap/servlet/JMapServlet.java
+++ jmap/jmap-core/src/main/java/net/javaonrails/webnav/jmap/servlet/JMapServlet.java
@@ -27,23 +27,23 @@ import org.apache.commons.logging.LogFactory;
 public class JMapServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-    
+
     private String prefixPackageName = "";
-    
+
     public static final String CONTROLLER_PACKAGE = "controller.";
-    
+
     public static final String BEAN_PACKAGE = "bean.";
 
     private String characterEncoding;
-    
+
     private String contentType;
-    
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public JMapServlet() {
         super();
-    // TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -54,49 +54,53 @@ public class JMapServlet extends HttpServlet {
         super.init(config);
         this.characterEncoding = config.getInitParameter("CharacterEncoding");
         this.contentType = config.getInitParameter("ContentType");
-        if(this.characterEncoding == null) 
+        if (this.characterEncoding == null) {
             this.characterEncoding = "UTF-8";
-        if(this.contentType == null)
+        }
+        if (this.contentType == null) {
             this.contentType = "text/html; charset=" + characterEncoding;
+        }
         initJMap();
     }
-    
+
     /**
      * This method cam be called by controller to re-initialize the config file.
-     * Or, you may want to override this function.
-     * This method get the /WEB-INF/jmap-config.properties through the 
+     * Or, you may want to override this function. This method get the
+     * /WEB-INF/jmap-config.properties through the
      * org.apache.commons.configuration.PropertiesConfiguration
      */
     public void initJMap() {
         try {
-        PropertiesConfiguration config = new PropertiesConfiguration();
-        config.load(getServletContext()
-                .getResourceAsStream("/WEB-INF/jmap-config.properties"));
-        setPrefixPackageName(config.getString("jmap.controller.package", ""));
+            PropertiesConfiguration config = new PropertiesConfiguration();
+            config.load(getServletContext()
+                    .getResourceAsStream("/WEB-INF/jmap-config.properties"));
+            setPrefixPackageName(config.getString("jmap.controller.package", ""));
         } catch (Exception e) {
             getLogger().info("/WEB-INF/jmap-config.properties is not required\r\n"
                     + "But if you define a key: jmap.controller.package in it "
                     + "will be better to orgernize the packages.", e);
         }
     }
-    
+
     private static Log logger = LogFactory.getLog(JMapServlet.class);
+
     public static Log getLogger() {
         return logger;
     }
 
     /**
      * You can override this method for other different behave.
+     *
      * @param cb is a annotation class of CreateBean
      * @param controller the controller class instance
      * @throws java.lang.reflect.InvocationTargetException
      * @throws java.lang.InstantiationException
      * @throws java.lang.IllegalAccessException
      */
-    protected BasicBean createBean(CreateBean cb, ControllerBase controller) 
-            throws InvocationTargetException, 
+    protected BasicBean createBean(CreateBean cb, ControllerBase controller)
+            throws InvocationTargetException,
             InstantiationException, IllegalAccessException {
-        
+
         String controllerClassName = controller.getClass().getSimpleName();
         String beanClassname = null;
         String attrName = null;
@@ -104,19 +108,21 @@ public class JMapServlet extends HttpServlet {
         BasicBean bean = null;
         if (cb == null) {
             beanClassname = getBeanClassName(controller, controller.getActionMethod());
-            if(logger.isDebugEnabled())
-                logger.debug("CreateBean is null, default bean class name is " 
-                    + beanClassname);
+            if (logger.isDebugEnabled()) {
+                logger.debug("CreateBean is null, default bean class name is "
+                        + beanClassname);
+            }
             attrName = controllerClassName + controller.getActionMethod();
-            if(logger.isDebugEnabled())
+            if (logger.isDebugEnabled()) {
                 logger.debug("The bean Request attribute name is " + attrName);
+            }
         } else if (cb.create()) {
             httpm = cb.createOnHttpMethod();
-            beanClassname = StringUtils.isEmpty(cb.beanClassName()) ? 
-                getBeanClassName(controller, controller.getActionMethod()) : cb.beanClassName();
-            attrName = StringUtils.isEmpty(cb.requestAttrName()) ? 
-                controllerClassName + controller.getActionMethod() : cb.requestAttrName();
-            if(logger.isDebugEnabled()) {
+            beanClassname = StringUtils.isEmpty(cb.beanClassName())
+                    ? getBeanClassName(controller, controller.getActionMethod()) : cb.beanClassName();
+            attrName = StringUtils.isEmpty(cb.requestAttrName())
+                    ? controllerClassName + controller.getActionMethod() : cb.requestAttrName();
+            if (logger.isDebugEnabled()) {
                 logger.debug("The CreateBean is true, bean class name is "
                         + beanClassname);
                 logger.debug("The bean Request attribute name is " + attrName);
@@ -125,17 +131,18 @@ public class JMapServlet extends HttpServlet {
         try {
             boolean createOnHttpMethod = false;
             String httpMethod = controller.getRequest().getMethod();
-            if(httpm != null) {
-                for(String method : httpm) {
-                    if(httpMethod.equals(method)) {
+            if (httpm != null) {
+                for (String method : httpm) {
+                    if (httpMethod.equals(method)) {
                         createOnHttpMethod = true;
                         break;
                     }
                 }
             }
-            if(logger.isDebugEnabled())
+            if (logger.isDebugEnabled()) {
                 logger.debug("the createOnHttpMethod is " + Arrays.toString(httpm)
-                    + ". The current HTTP method is " + httpMethod + ".");
+                        + ". The current HTTP method is " + httpMethod + ".");
+            }
             if (StringUtils.isNotEmpty(beanClassname) && createOnHttpMethod) {
                 bean = createBean(controller, beanClassname);
                 controller.getRequest().setAttribute(attrName, bean);
@@ -146,23 +153,24 @@ public class JMapServlet extends HttpServlet {
         }
         return bean;
     }
+
     /**
-     * 
-     * create and initialize the bean by the default rule.
-     * the rule to find the bean is: packagePrefix + BEAN_PACKAGE + beanSimpleName
-     * create this instance and call the abstract method init()
-     * if the beanSimpleName contains '.', this name will be treated as full class
-     * name, so the beanSimpleName will not add the package prefix name.
-     * 
-     * Mar 8th 2014: replace the Class.forName to getClassLoader.loadClass
-     * This is because the OSGi recommendation for classloader.
-     * refer to: http://blog.bjhargrave.com/2007/09/classforname-caches-defined-class-in.html
-     * 
+     *
+     * create and initialize the bean by the default rule. the rule to find the
+     * bean is: packagePrefix + BEAN_PACKAGE + beanSimpleName create this
+     * instance and call the abstract method init() if the beanSimpleName
+     * contains '.', this name will be treated as full class name, so the
+     * beanSimpleName will not add the package prefix name.
+     *
+     * Mar 8th 2014: replace the Class.forName to getClassLoader.loadClass This
+     * is because the OSGi recommendation for classloader. refer to:
+     * http://blog.bjhargrave.com/2007/09/classforname-caches-defined-class-in.html
+     *
      * @param beanSimpleName, this is a simple class name, has no package name
      * @return the BasicBean instance
      */
-    public BasicBean createBean(ControllerBase controller, String beanSimpleName) 
-            throws InstantiationException, IllegalAccessException, 
+    public BasicBean createBean(ControllerBase controller, String beanSimpleName)
+            throws InstantiationException, IllegalAccessException,
             ClassNotFoundException, InvocationTargetException {
         BasicBean bean = null;
         String classname = getBeanClassName(controller, beanSimpleName);
@@ -172,11 +180,11 @@ public class JMapServlet extends HttpServlet {
         bean.execute();
         return bean;
     }
+
     /**
-     * return the class name in String[0], 
-     * action method name in String[1],
-     * hint in the String[2]
-     * 
+     * return the class name in String[0], action method name in String[1], hint
+     * in the String[2]
+     *
      * @param request
      * @return
      */
@@ -208,17 +216,16 @@ public class JMapServlet extends HttpServlet {
     }
 
     /**
-     * You can override this function to define a new way to find the 
-     * controller's class name.
-     * If this application use the `/' as the servlet path, the Class Root
-     * will be mapped.
-     * The config file /WEB-INF/jmap-config.properties will define a key:
+     * You can override this function to define a new way to find the
+     * controller's class name. If this application use the `/' as the servlet
+     * path, the Class Root will be mapped. The config file
+     * /WEB-INF/jmap-config.properties will define a key:
      * jmap.controller.package, this key will keep the package prefix name for
      * the controller Class.
-     * 
+     *
      * @param servletPath
-     * @return the full name with package name and you can use ClassLoader.loadClass
-     * to find the Class.
+     * @return the full name with package name and you can use
+     * ClassLoader.loadClass to find the Class.
      */
     protected String findControllerClassName(String servletPath) {
         if (servletPath.length() == 0) {
@@ -228,15 +235,16 @@ public class JMapServlet extends HttpServlet {
     }
 
     public String getBeanClassName(ControllerBase controller, String beanName) {
-        return (beanName.indexOf('.') > -1) ? 
-            beanName : 
-            this.getPrefixPackageName() 
-            + BEAN_PACKAGE + controller.getClass().getSimpleName().toLowerCase() 
-            + "." + beanName;
+        return (beanName.indexOf('.') > -1)
+                ? beanName
+                : this.getPrefixPackageName()
+                + BEAN_PACKAGE + controller.getClass().getSimpleName().toLowerCase()
+                + "." + beanName;
     }
 
     /**
-     * get the prefix name from the web.xml? 
+     * get the prefix name from the web.xml?
+     *
      * @return the controller and other class package's prefix name
      */
     public String getPrefixPackageName() {
@@ -247,9 +255,9 @@ public class JMapServlet extends HttpServlet {
     public void setPrefixPackageName(String prefixPackageName) {
         this.prefixPackageName = prefixPackageName;
     }
-    
+
     /**
-     * 
+     *
      * @param request
      * @param response
      * @throws ServletException
@@ -267,7 +275,7 @@ public class JMapServlet extends HttpServlet {
 
             String[] clz = getAction(request);
 
-            Class controllerClass = this.getClass().getClassLoader().loadClass(getPrefixPackageName() 
+            Class controllerClass = this.getClass().getClassLoader().loadClass(getPrefixPackageName()
                     + CONTROLLER_PACKAGE + clz[0]);
 
             Method m = controllerClass.getMethod(clz[1]);
@@ -282,9 +290,9 @@ public class JMapServlet extends HttpServlet {
             // modified on Jul 21st 2011 the return value is true then these 
             // actions will go on 
             if (controller.init()) {
-	            createBean(m.getAnnotation(CreateBean.class), 
-	                    controller);
-	            m.invoke(oc);
+                createBean(m.getAnnotation(CreateBean.class),
+                        controller);
+                m.invoke(oc);
             }
         } catch (ClassNotFoundException e) {
             // 404 response
@@ -317,16 +325,15 @@ public class JMapServlet extends HttpServlet {
         }
 
     }
-    
+
     /**
-     * This method will change the uri name to some other way.
-     * You may override this function to supply another translate method.
-     * By default, this method will translate the uri to capitalize way 
-     * and replace the '.', ' ', '-' and '_' to empty string and cpaitalize 
-     * the next character.
-     * So, the uri like /index.html will be translate to IndexHtml, 
-     * login-page will be LoginPage
-     * 
+     * This method will change the uri name to some other way. You may override
+     * this function to supply another translate method. By default, this method
+     * will translate the uri to capitalize way and replace the '.', ' ', '-'
+     * and '_' to empty string and cpaitalize the next character. So, the uri
+     * like /index.html will be translate to IndexHtml, login-page will be
+     * LoginPage
+     *
      * @param uri
      * @return
      */
@@ -340,9 +347,11 @@ public class JMapServlet extends HttpServlet {
     }
 
     /**
+     * @param request
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * response)
      */
+    @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         this.processRequest(request, response);
@@ -350,10 +359,23 @@ public class JMapServlet extends HttpServlet {
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * response)
      */
+    @Override
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
+        this.processRequest(request, response);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        this.processRequest(request, response);
+    }
+    
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
         this.processRequest(request, response);
     }
 }
